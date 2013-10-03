18f63c5ca98b01f58d6dd7c88397362691c90a69
Makky <makky.4d6b.3f5@gmail.com>
2013-10-03 17:07:29 +0900

    creditを追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-(function(){
-	alert(1);
-})(); 
+$(window).load(function (){
+
+var credit = 0;
+
+function credit_add(){
+	credit++;
+	console.log(credit);
+}
+
+function drawCredit(){
+	$("#CreditDispley").html(credit);
+}
+
+var event = new jQuery.Event("click");
+$("#credit").click(function(event){
+  //alert(event.type);
+  credit_add();
+  drawCredit();
+});
+
+});
+
