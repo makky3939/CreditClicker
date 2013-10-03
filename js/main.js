d61afc7642f5c5e223ed812594110bd0dc3f430f
Makky <makky.4d6b.3f5@gmail.com>
2013-10-03 18:06:41 +0900

    countを表示出来るように

--- js/main.js
+++ js/main.js
@@ -2,6 +2,26 @@ $(window).load(function (){
 
 var credit = 0;
 
+var items = [
+	{
+		name:"hoge",
+		price:100,
+		effect:0.1
+	},
+	{
+		name:"hoge2",
+		price:200,
+		effect:0.2
+	}
+];
+
+function check_items(){
+	console.log(items);
+}
+
+check_items();
+
+
 function credit_add(){
 	credit++;
 	console.log(credit);
